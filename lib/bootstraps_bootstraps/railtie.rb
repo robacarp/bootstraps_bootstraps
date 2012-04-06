7a2fc53ae8efbda274a2639eafac40a019b4f74f
Robert L. Carpenter <robacarp@gmail.com>
2012-04-06 16:58:20 -0600

    Changes the railtie load order.
    ActionMailer initializer seems to cause a bug if it is 
    loaded before the railtie.  This needs more investigating.

--- lib/bootstraps_bootstraps/railtie.rb
+++ lib/bootstraps_bootstraps/railtie.rb
@@ -9,9 +9,9 @@ module BootstrapsBootstraps
       application.config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     end
 
-    config.to_prepare do
-      #RELOAD THE GEM ALL THE TIME WITHOUT RESTARTING THE SERVER :O (without restarting the server)
+    config.before_initialize do
       ::ActionView::Helpers.send(:load, 'bootstraps_bootstraps/bootstrap_form_builder.rb')
+      ::ActionView::Helpers.send(:load, 'bootstraps_bootstraps/bootstrap_form_helper.rb')
       ::ActionView::Helpers.send(:include, BootstrapsBootstraps::BootstrapFormHelper)
     end
   end
