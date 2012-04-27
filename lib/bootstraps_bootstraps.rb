8268965ceea82159d2cdb728df8f5055e2def813
Robert L. Carpenter <robacarp@gmail.com>
2012-04-27 12:10:57 -0600

    Okay railties irritate me.
    change nothing? broken. >:

--- lib/bootstraps_bootstraps.rb
+++ lib/bootstraps_bootstraps.rb
@@ -9,3 +9,7 @@ module BootstrapsBootstraps
   autoload :BootstrapFormBuilder
   autoload :BootstrapFormHelper
 end
+
+::ActionView::Helpers.send(:load, 'bootstraps_bootstraps/bootstrap_form_builder.rb')
+::ActionView::Helpers.send(:load, 'bootstraps_bootstraps/bootstrap_form_helper.rb')
+::ActionView::Helpers.send(:include, BootstrapsBootstraps::BootstrapFormHelper)
--- lib/bootstraps_bootstraps/railtie.rb
+++ lib/bootstraps_bootstraps/railtie.rb
@@ -6,11 +6,5 @@ module BootstrapsBootstraps
       # Credit where due: http://stackoverflow.com/questions/5267998/rails-3-field-with-errors-wrapper-changes-the-page-appearance-how-to-avoid-t
       application.config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     end
-
-    config.before_initialize do
-      ::ActionView::Helpers.send(:load, 'bootstraps_bootstraps/bootstrap_form_builder.rb')
-      ::ActionView::Helpers.send(:load, 'bootstraps_bootstraps/bootstrap_form_helper.rb')
-      ::ActionView::Helpers.send(:include, BootstrapsBootstraps::BootstrapFormHelper)
-    end
   end
 end
