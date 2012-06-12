34ccd3cae58e7c4926c0f98f3ac40e6cecb42296
Robert L. Carpenter <robacarp@gmail.com>
2012-06-12 11:16:54 -0600

    Build for later rails's, finish moving engine->tie

--- lib/bootstraps_bootstraps/engine.rb
+++ lib/bootstraps_bootstraps/engine.rb
@@ -1,12 +1,12 @@
-module BootstrapsBootstraps
-  class Engine < ::Rails::Engine
-
-    initializer 'bootstraps_bootstraps.initialize' do
-      ActiveSupport.on_load :action_view do
-        include BootstrapsBootstraps::BootstrapFormHelper
-      end
-    end
-
-  end
-end
-
+#module BootstrapsBootstraps
+#  class Engine < ::Rails::Engine
+#
+#    config.before_initialize do
+#      ActiveSupport.on_load :action_view do
+#        include BootstrapsBootstraps::BootstrapFormHelper
+#      end
+#    end
+#
+#  end
+#end
+#
--- lib/bootstraps_bootstraps/railtie.rb
+++ lib/bootstraps_bootstraps/railtie.rb
@@ -1,10 +1,15 @@
 module BootstrapsBootstraps
   class Railtie < Rails::Railtie
+
     initializer "bootstraps_bootstraps.configure_rails_initialization" do |application|
       # Prevent Rails from wrapping form elements with errors in <div class="field_with_error">
       # boxes.  Bootstrap has a bit of a different paradigm.
       # Credit where due: http://stackoverflow.com/questions/5267998/rails-3-field-with-errors-wrapper-changes-the-page-appearance-how-to-avoid-t
       application.config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+      ActiveSupport.on_load :action_view do
+        include BootstrapsBootstraps::BootstrapFormHelper
+      end
+
     end
   end
 end
